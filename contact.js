2e2c57f2d73486a388503eaca1505555f4ee9936
helder-hanka <hfernandes238@gmail.com>
2020-03-20 01:32:10 +0100

    mofified and add Js

---
+++ contact.js
@@ -0,0 +1,20 @@
+function validateForm() {
+    let x = document.forms["myForm"]["fname"].value;
+    let y = document.forms["myForm"]["lastname"].value;
+    let z = document.forms["myForm"]["subject"].value;
+    if ( x == "" || y == "" || z =="") {
+    alert(" Your Name and Lastname and message please !");
+    return false;
+    } else if (x == "") {
+    alert("Your name please !")
+    return false;
+    }else if (y == "") {
+      alert("Your lastname please !");
+      return false;
+    } else if (z == "") {
+        alert("Your message please !");
+        return false;
+    } else {
+    alert(" To Sen");
+    }
+  }
